aebda3e27fad68b4941c78fa1059d4601c62ea6f
GeroRS33 <gerorevetria2@gmail.com>
2026-05-12 17:45:46 -0300

    Cambio Input Field

--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -1,6 +1,15 @@
 import "./InputField.css";
 
-function InputField({ label, type = "text", placeholder, icon, rightIcon }) {
+function InputField({
+  label,
+  type = "text",
+  placeholder,
+  icon,
+  rightIcon,
+  register,
+  nombre,
+  error,
+}) {
   return (
     <div className="input-field">
       <label>{label}</label>
@@ -8,12 +17,18 @@ function InputField({ label, type = "text", placeholder, icon, rightIcon }) {
       <div className="input-box">
         {icon && <img className="input-icon" src={icon} alt="" />}
 
-        <input type={type} placeholder={placeholder} />
+        <input
+          type={type}
+          placeholder={placeholder}
+          {...register(nombre)}
+        />
 
         {rightIcon && (
           <img className="input-right-icon" src={rightIcon} alt="" />
         )}
       </div>
+
+      {error && <p className="input-error">{error.message}</p>}
     </div>
   );
 }
